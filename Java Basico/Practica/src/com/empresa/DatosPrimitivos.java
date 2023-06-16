51fd8f9e8e51a2de8d98a41b35f334b74def185b
yetogi0 <127547244+yetogi0@users.noreply.github.com>
2023-06-16 12:21:23 -0500

    Add files via upload

---
+++ Java Basico/Practica/src/com/empresa/DatosPrimitivos.java
@@ -0,0 +1,22 @@
+package com.empresa;
+
+public class DatosPrimitivos {
+
+
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        long numeroLargo = 1234567890L;
+        double numeroDecimal = 45.12;
+        float  numeroDecimalCorto = 1.2f;
+        boolean boleano = true;
+        char caracter = 'n';
+        String  cadena = "Hola ";
+
+        System.out.println("la informacion es: " + numero + " , " + numeroLargo + " , " + numeroDecimal + " ," + numeroDecimalCorto+ " , " + boleano + " , "+ cadena);
+    }
+
+
+
+}
